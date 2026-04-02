95818175e9564d35dfd194fb194a3c4a5b881af2
Tristan Rimmer <Tristan Rimmer>
2026-04-02 12:32:40 +0100

    Added more albums

--- top_albums.js
+++ top_albums.js
@@ -1,5 +1,4 @@
-
-  const album_songs = {
+const album_songs = {
       "codename-x": {
         album_name: "Codename X",
         songs: [
@@ -24,7 +23,7 @@
             run_time: "3:25"
         },
         {
-            name: "Interstellar",
+                name: "Interstellar",
             run_time: "4:03"
         },
         {
@@ -49,16 +48,124 @@
         }
       ]
     },
+    "onyx" : {
+        album_name: "Onyx",
+        songs: [
+            { name: "Decimate", run_time: "3:46" },
+            { name: "Osiris", run_time: "4:08" },
+            { name: "Cough Drop", run_time: "3:12" },
+            { name: "Back To Back", run_time: "3:18" },
+            { name: "Run This", run_time: "3:25" },
+            { name: "Plague Doctor", run_time: "4:02" },
+            { name: "Demisaur", run_time: "3:36" },
+            { name: "Name Drop", run_time: "3:07" },
+            { name: "The Last Elder", run_time: "4:41" },
+            { name: "Our Fire", run_time: "4:16" },
+            { name: "Temporary Blue", run_time: "4:22" },
+            { name: "Salvation", run_time: "4:35" },
+            { name: "Lockdown", run_time: "3:28" },
+            { name: "Fall Apart", run_time: "4:11" }
+        ]
+    },
+    "virus" : {
+        album_name: "Virus",
+        songs: [
+            { name: "Virus", run_time: "3:45" },
+            { name: "X Up", run_time: "3:31" },
+            { name: "Throwin' Elbows", run_time: "3:42" },
+            { name: "Rumble", run_time: "4:42" },
+            { name: "Reload", run_time: "3:53" },
+            { name: "Home", run_time: "4:02" },
+            { name: "With You", run_time: "4:20" },
+            { name: "Final Boss", run_time: "3:58" },
+            { name: "Neck Brace", run_time: "3:34" },
+            { name: "Drop Bombs", run_time: "3:40" },
+            { name: "Live Wire", run_time: "4:08" },
+            { name: "Before The Sun", run_time: "4:36" },
+            { name: "Interlude", run_time: "1:42" },
+            { name: "The Paradox", run_time: "4:12" }
+        ]
+    },
+    "x-rated" : {
+        album_name: "X Rated",
+        songs: [
+            { name: "X Rated", run_time: "4:45" },
+            { name: "Ohhh Nooo", run_time: "4:28" },
+            { name: "Execute", run_time: "4:12" },
+            { name: "Get To The Point", run_time: "3:51" },
+            { name: "8 Bit Superhero", run_time: "4:03" },
+            { name: "Boom", run_time: "4:07" },
+            { name: "Subsonic", run_time: "5:10" },
+            { name: "Sleepless", run_time: "5:22" },
+            { name: "Sexism", run_time: "4:18" },
+            { name: "The Underground", run_time: "4:34" },
+            { name: "Hydra", run_time: "4:50" },
+            { name: "Deviance", run_time: "5:05" }
+        ]
+    },
     "apex" : {
         album_name: "Apex",
+
         songs: [
+            {
+                name: "Exterminate",
+                run_time: "3:34"
+            },
+            {
+                name: "1 On 1",
+                run_time: "3:19"
+            },
+            {
+                name: "Wake Up",
+                run_time: "5:39"
+            },
+            {
+                name: "Tonight",
+                run_time: "4:31"
+            },
+            {
+                name: "Gold (Stupid Love)",
+                run_time: "4:55"
+            },
+            {
+                name: "Fall",
+                run_time: "3:56"
+            },
+            {
+                name: "Home",
+                run_time: "3:57"
+            },
+            {
+                name: "Where Are You",
+                run_time: "3:05"
+            },
+            {
+                name: "Power",
+                run_time: "4:02"
+            },
+            {
+                name: "Vault",
+                run_time: "4:57"
+            },
+            {
+                name: "Die For You",
+                run_time: "3:59"
+            },
+            {
+                name: "Hoods Up",
+                run_time: "4:00"
+            },
+            {
+                name: "Fight Through The Pain",
+                run_time: "4:11"
+            },
             {
                 name: "Rumble",
                 run_time: "4:42"
             }
         ]
     }
-  };
+};
 
 
 function load_popup(album_name) {
